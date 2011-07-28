8647a134b12f772a484234e3b80589da0a1dd4d4
Yosem Sweet <yosem.sweet@suite101.com>
2011-07-28 14:08:12 -0700

    we now support comments_for tag and have set up a generator to create the facebook account yml file.

--- lib/acts_as_opengraph/helper/acts_as_opengraph_helper.rb
+++ lib/acts_as_opengraph/helper/acts_as_opengraph_helper.rb
@@ -32,7 +32,7 @@ module ActsAsOpengraphHelper
     href = options[:href] ? options[:href] : obj.opengraph_url
     return unless href.present?
 
-    config = { :layout => :standard, :show_faces => false, :width => 450, :action => :like, :colorscheme => :light }
+    config = { :layout => :standard, :show_faces => false, :width => 450, :action => :like, :colorscheme => :light, :appid => FACEBOOK_CONFIG[:appId], :locale => 'en_US' }
     config.update(options) if options.is_a?(Hash)
 
     o_layout = config[:layout].to_sym
@@ -43,11 +43,10 @@ module ActsAsOpengraphHelper
     elsif o_layout == :box_count
       config[:height] = 65
     end
-    config[:locale] ||= 'en_US'
 
     if config[:xfbml]
       unless @fb_sdk_included
-        content_for :javascripts, fb_javascript_include_tag( config[:locale], config[:appid] )
+        content_for :javascripts, fb_javascript_include_tag( config[:appid], config[:locale] )
       end
       like_html = %(<fb:like href="#{CGI.escape(href)}" layout="#{config[:layout]}" show_faces="#{config[:show_faces]}" action="#{config[:action]}" colorscheme="#{config[:colorscheme]}" width="#{config[:width]}" height="#{config[:height]}" font="#{config[:font]}"></fb:like>)
     else
@@ -62,32 +61,20 @@ module ActsAsOpengraphHelper
     href = options[:href] ? options[:href] : obj.opengraph_url
     return unless href.present?
 
-		onfig = { :layout => :standard, :show_faces => false, :width => 450, :action => :like, :colorscheme => :light }
+		config = { :width => 400, :num_posts => 2, :colorscheme => :light, :appid => FACEBOOK_CONFIG[:appId], :locale => 'en_US' }
     config.update(options) if options.is_a?(Hash)
 
-    o_layout = config[:layout].to_sym
-    if o_layout == :standard
-      config[:height] = config[:show_faces].to_s.to_sym == :true ? 80 : 35
-    elsif o_layout == :button_count
-      config[:height] = 21
-    elsif o_layout == :box_count
-      config[:height] = 65
-    end
     config[:locale] ||= 'en_US'
 
-    if config[:xfbml]
-      unless @fb_sdk_included
-        content_for :javascripts, fb_javascript_include_tag( config[:locale], config[:appid] )
-      end
-      like_html = %(<fb:like href="#{CGI.escape(href)}" layout="#{config[:layout]}" show_faces="#{config[:show_faces]}" action="#{config[:action]}" colorscheme="#{config[:colorscheme]}" width="#{config[:width]}" height="#{config[:height]}" font="#{config[:font]}"></fb:like>)
-    else
-      like_html = %(<iframe src="http://www.facebook.com/plugins/like.php?href=#{CGI.escape(href)}&amp;layout=#{config[:layout]}&amp;show_faces=#{config[:show_faces]}&amp;width=#{config[:width]}&amp;action=#{config[:action]}&amp;colorscheme=#{config[:colorscheme]}&amp;height=#{config[:height]}" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:#{config[:width]}px; height:#{config[:height]}px;" allowTransparency="true"></iframe>)
+    unless @fb_sdk_included
+    	content_for :javascripts, fb_javascript_include_tag( config[:appid], config[:locale] )
     end
-
-		like_html.html_safe
+    comments_html = %(<div id=\"fb-root\"></div><fb:comments href=\"#{CGI.escape(href)}\" num_posts=\"#{config[:num_posts]}\" width=\"#{config[:width]}\" colorscheme=\"#{config[:colorscheme]}\"></fb:comments>)
+  
+		comments_html.respond_to?(:html_safe) ? comments_html.html_safe : comments_html
   end
 
-  def fb_javascript_include_tag(appid='', locale='en_US')
+  def fb_javascript_include_tag(appid=FACEBOOK_CONFIG[:appId], locale='en_US')
     @fb_sdk_included = true
     async_fb = <<-END
       <div id="fb-root"></div>
